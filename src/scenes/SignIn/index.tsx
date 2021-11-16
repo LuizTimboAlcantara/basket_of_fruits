02ad9c60c8cff3457c8f0a721f0644f0d88feb20
Luiz Timbó <60242867+LuizTimboAlcantara@users.noreply.github.com>
2021-11-16 14:11:34 -0300

    Iniciandoa tela de login

--- src/scenes/SignIn/index.tsx
+++ src/scenes/SignIn/index.tsx
@@ -1,12 +1,40 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Image} from 'react-native';
 
-import {Container} from './styles';
+import LogoPng from '../../assets/img/logo.png';
+
+import {
+  Container,
+  Header,
+  TitleWrapper,
+  Title,
+  SignInTitle,
+  Footer,
+  FooterWrapper,
+} from './styles';
 
 export function SignIn() {
   return (
     <Container>
-      <Text>SigIn</Text>
+      <Header>
+        <TitleWrapper>
+          <Image
+            source={LogoPng}
+            style={{height: 110, width: 110}}
+            resizeMode="contain"
+          />
+        </TitleWrapper>
+
+        <Title>Todo dia {'\n'} é dia de feira</Title>
+        <SignInTitle>
+          Selecionamos as melhoras {'\n'}
+          frutas para você
+        </SignInTitle>
+      </Header>
+
+      <Footer>
+        <FooterWrapper></FooterWrapper>
+      </Footer>
     </Container>
   );
 }
--- src/scenes/SignIn/styles.ts
+++ src/scenes/SignIn/styles.ts
@@ -1,3 +1,47 @@
 import styled from 'styled-components/native';
+import {RFValue, RFPercentage} from 'react-native-responsive-fontsize';
 
-export const Container = styled.View``;
+import Colors from '../../utils/theme/colors';
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Header = styled.View`
+  width: 100%;
+  height: 70%;
+  background-color: ${Colors.primary};
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+export const TitleWrapper = styled.View`
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  color: ${Colors.shape};
+  font-size: ${RFValue(30)}px;
+  text-align: center;
+  margin-top: ${RFValue(45)}px;
+`;
+
+export const SignInTitle = styled.Text`
+  color: ${Colors.shape};
+  font-size: ${RFValue(16)}px;
+  text-align: center;
+  margin-top: ${RFValue(80)}px;
+  margin-bottom: ${RFValue(67)}px;
+`;
+
+export const Footer = styled.View`
+  width: 100%;
+  height: 30%;
+  background-color: ${Colors.secondary};
+`;
+
+export const FooterWrapper = styled.View`
+  margin-top: ${RFPercentage(-4)}px;
+  padding: 0 ${RFValue(32)}px;
+  justify-content: space-between;
+`;
